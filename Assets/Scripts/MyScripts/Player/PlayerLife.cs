34489d578831b83ce592f31bb71e9234cd5e259e
Richard Lopes <richard.lopes1995@gmail.com>
2022-10-06 23:16:56 -0300

    test
    Former-commit-id: e5cfcd9ac3acf02e9580d719a778471dd11057d7

--- Assets/Scripts/MyScripts/Player/PlayerLife.cs
+++ Assets/Scripts/MyScripts/Player/PlayerLife.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class PlayerLife : MonoBehaviour {
-    public Text text;
+    public Text text ;
     // Start is called before the first frame update
     void Start() {
         text = GetComponent<Text>();
